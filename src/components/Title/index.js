24c698886b9eda582f9f192e34cca5f2a051117b
radubogdan6466 <radubogdan6466@gmail.com>
2023-06-03 23:00:04 +0300

    Removed secont title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import {Text} from 'react-native';
 import styles from './styles';
 const Title = () => {
-  return <Text style={styles.title}>Title component</Text>;
+  return <Text style={styles.title}>Title</Text>;
 };
 export default Title;
